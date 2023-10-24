fd210d75a0b845cd271b7ef7a5175c0f690fe80b
ashmatok <ashmatok@gmail.com>
2023-10-24 12:42:02 +0300

    hw with error gradle

---
+++ .idea/src/SearchTests.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+/**
+ Created by anton on 24.10.2023
+ */public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
